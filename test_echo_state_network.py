965f0bda2b769514a4d8f33b62b14c43ccc7ac8d
aconkey <adam.conkey@gmail.com>
2015-06-22 21:09:01 -0500

    Add test for simulation run

--- test_echo_state_network.py
+++ test_echo_state_network.py
@@ -24,16 +24,53 @@ class EchoStateNetworkTests(unittest.TestCase):
         self.assertLess(np.amin(w3.data), -1.0)
         self.assertGreater(np.amax(w3.data), 1.0)
 
-    def test_scale_spectral_radius(self):
-        w1 = esn.initialize_weights(10, 10)
-        w1 = esn.scale_spectral_radius(w1)
+    def test_initialize_reservoir(self):
+        w1 = esn.initialize_reservoir(10)
         self.assertAlmostEqual(1.0, max(abs(eigs(w1)[0])))
-        w2 = esn.initialize_weights(10, 10)
-        w2 = esn.scale_spectral_radius(w2, 5.0)
+        w2 = esn.initialize_reservoir(10, spec_rad=5.0)
         self.assertAlmostEqual(5.0, max(abs(eigs(w2)[0])))
-        w3 = esn.initialize_weights(10, 10)
-        w3 = esn.scale_spectral_radius(w3, 0.1)
+        w3 = esn.initialize_reservoir(10, spec_rad=0.1)
         self.assertAlmostEqual(0.1, max(abs(eigs(w3)[0])))
+
+    def test_compute_accuracy(self):
+        e1 = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        e2 = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+        e3 = np.array([1, 1, 1, 1, 1, 1, 1, 0, 0, 0])
+        e4 = np.array([0, 0, 0, 0, 0, 0, 0, 1, 1, 1])
+        e5 = np.array([1, 1, 0, 1, 0, 0, 1, 0, 0, 1])
+        a1 = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        a2 = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+        a3 = np.array([1, 1, 1, 1, 1, 1, 1, 0, 0, 0])
+        a4 = np.array([0, 0, 0, 0, 0, 0, 0, 1, 1, 1])
+        a5 = np.array([1, 0, 0, 1, 0, 1, 1, 1, 0, 0])
+        self.assertAlmostEqual(1.0, esn.compute_accuracy(e1, a1))
+        self.assertAlmostEqual(1.0, esn.compute_accuracy(e2, a2))
+        self.assertAlmostEqual(1.0, esn.compute_accuracy(e3, a3))
+        self.assertAlmostEqual(1.0, esn.compute_accuracy(e4, a4))
+        self.assertAlmostEqual(0.7, esn.compute_accuracy(e3, a2))
+        self.assertAlmostEqual(0.7, esn.compute_accuracy(e4, a1))
+        self.assertAlmostEqual(0.0, esn.compute_accuracy(e1, a2))
+        self.assertAlmostEqual(0.0, esn.compute_accuracy(e2, a1))
+        self.assertAlmostEqual(0.0, esn.compute_accuracy(e3, a4))
+        self.assertAlmostEqual(0.0, esn.compute_accuracy(e4, a3))
+        self.assertAlmostEqual(0.6, esn.compute_accuracy(e5, a5))
+
+    def test_run_simulation(self):
+        inputs = np.random.rand(10, 10)
+        targets = np.array([[1],
+                            [1],
+                            [1],
+                            [0],
+                            [0],
+                            [0],
+                            [1],
+                            [1],
+                            [1],
+                            [1]])
+        data = (inputs, targets)
+        network = esn.EchoStateNetwork()
+        print 'Running simulation:'
+        esn.run_simulation(network, data)
 
 def main():
     unittest.main()
